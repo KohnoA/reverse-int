f9bc8c0c92c761ec6b08cbde44d6026f2d09d884
KohnoA <san.kohno@yandex.ru>
2022-06-28 22:16:14 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const numUp = Math.abs(n);
+    const numStr = String(numUp);
+    const result = numStr.split('').reverse().join('');
+    return Number(result);
 }
